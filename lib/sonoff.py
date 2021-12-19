e33bd96fe68c97779be68ced207348e6a32affc2
GKar <demkon88@mail.ru>
2021-12-19 22:24:08 +0300

    add relay2 & ligths page

--- lib/sonoff.py
+++ lib/sonoff.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 import requests
 from lib.config import config
 from lib.models.relay_data import RelayData
@@ -5,8 +7,13 @@ from lib.models.relay_data import RelayData
 _timeout = 2
 
 
-def get_relay_data() -> RelayData:
-    addr = config['RELAY_ADDR']
+class RelayType(Enum):
+    RELAY1 = 1
+    RELAY2 = 2
+
+
+def get_relay_data(relay_type: RelayType) -> RelayData:
+    addr = _get_relay_addr(relay_type)
     url = f'http://{addr}/zeroconf/info'
 
     request = {'deviceid': '', 'data': {}}
@@ -30,8 +37,8 @@ def get_relay_data() -> RelayData:
             signal_strength='Ошибка')
 
 
-def set_relay(new_status: bool):
-    addr = config['RELAY_ADDR']
+def set_relay(relay_type: RelayType, new_status: bool):
+    addr = _get_relay_addr(relay_type)
     url = f'http://{addr}/zeroconf/switch'
 
     switch_state = 'on' if new_status else 'off'
@@ -39,3 +46,13 @@ def set_relay(new_status: bool):
     request = {'deviceid': '', 'data': {'switch': switch_state}}
 
     requests.post(url, json=request, timeout=_timeout)
+
+
+def _get_relay_addr(relay_type: RelayType) -> str:
+    if relay_type == RelayType.RELAY1:
+        return config['RELAY1_ADDR']
+
+    if relay_type == RelayType.RELAY2:
+        return config['RELAY2_ADDR']
+
+    raise Exception('Unsupported relay type')
--- view/ctrl.py
+++ view/ctrl.py
@@ -1,6 +1,6 @@
-from flask import render_template, redirect, url_for, request, Blueprint
+from flask import render_template, redirect, request, Blueprint
 from lib.router import get_router_data, set_wifi, set_wifi_ext, set_rule, set_dimaphone_tunnel, set_demkon_tunnel
-from lib.sonoff import get_relay_data, set_relay
+from lib.sonoff import RelayType, get_relay_data, set_relay
 
 ctrl = Blueprint('ctrl', __name__)
 
@@ -8,51 +8,66 @@ ctrl = Blueprint('ctrl', __name__)
 @ctrl.route('/')
 def index():
     router_data = get_router_data()
-    relay_data = get_relay_data()
+    relay1_data = get_relay_data(RelayType.RELAY1)
+    relay2_data = get_relay_data(RelayType.RELAY2)
 
-    return render_template('index.html', router_data=router_data, relay_data=relay_data)
+    return render_template('index.html', router_data=router_data, relay1_data=relay1_data, relay2_data=relay2_data)
+
+
+@ctrl.route('/lights')
+def lights():
+    relay2_data = get_relay_data(RelayType.RELAY2)
+
+    return render_template('lights.html', relay2_data=relay2_data)
 
 
 @ctrl.route('/turn-rule', methods=['POST'])
 def turn_rule():
     new_status = _bool_parse(request.form['new_status'])
     set_rule(new_status)
-    return redirect(url_for('.index'))
+    return redirect(request.referrer)
 
 
 @ctrl.route('/turn-wifi', methods=['POST'])
 def turn_wifi():
     new_status = _bool_parse(request.form['new_status'])
     set_wifi(new_status)
-    return redirect(url_for('.index'))
+    return redirect(request.referrer)
 
 
 @ctrl.route('/turn-wifi-ext', methods=['POST'])
 def turn_wifi_ext():
     new_status = _bool_parse(request.form['new_status'])
     set_wifi_ext(new_status)
-    return redirect(url_for('.index'))
+    return redirect(request.referrer)
 
 
 @ctrl.route('/turn-dimaphone-tunnel', methods=['POST'])
 def turn_dimaphone_tunnel():
     new_status = _bool_parse(request.form['new_status'])
     set_dimaphone_tunnel(new_status)
-    return redirect(url_for('.index'))
+    return redirect(request.referrer)
 
 
 @ctrl.route('/turn-demkon-tunnel', methods=['POST'])
 def turn_demkon_tunnel():
     new_status = _bool_parse(request.form['new_status'])
     set_demkon_tunnel(new_status)
-    return redirect(url_for('.index'))
+    return redirect(request.referrer)
 
 
-@ctrl.route('/turn-relay', methods=['POST'])
-def turn_relay():
+@ctrl.route('/turn-relay1', methods=['POST'])
+def turn_relay1():
     new_status = _bool_parse(request.form['new_status'])
-    set_relay(new_status)
-    return redirect(url_for('.index'))
+    set_relay(RelayType.RELAY1, new_status)
+    return redirect(request.referrer)
+
+
+@ctrl.route('/turn-relay2', methods=['POST'])
+def turn_relay2():
+    new_status = _bool_parse(request.form['new_status'])
+    set_relay(RelayType.RELAY2, new_status)
+    return redirect(request.referrer)
 
 
 def _bool_parse(value: str) -> bool:
